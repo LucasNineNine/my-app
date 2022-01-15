4d46c8edec8705e396bee06fbff5bf3fc10f4b02
from_lucasfs <lucasfernandesfs@gmail.com>
2022-01-15 15:06:21 -0300

    15-01-22

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import React from 'react'
 import Nav from './nav'
 import Logo from './logo'
 
+const tempoElement = <h5>It is {new Date().toLocaleTimeString()}</h5>
 
+function Tempo() {
+    <div>
+        {tempoElement}
+    </div>
+}
+
+setInterval(Tempo, 1000)
 
 class App extends React.Component{
     render(){
@@ -14,6 +22,7 @@ class App extends React.Component{
                 <Logo  />
                 {/*<Hello  />*/}
                 <Nav  />
+                <Tempo />
             </div>
         );
     }
